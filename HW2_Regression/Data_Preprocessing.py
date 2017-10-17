c68bdc29decdcd675288c4289dd1bac64c1a92db
Hsin-Yi Chen <hychen.iim05g@g2.nctu.edu.tw>
2017-10-17 19:08:56 +0800

    HW2 Done!

--- HW2_Regression/Data_Preprocessing.py
+++ HW2_Regression/Data_Preprocessing.py
@@ -19,12 +19,14 @@ data = data.drop('EASE-MENT', axis = 1)
 data = data.drop('APARTMENT NUMBER', axis = 1)
 data = data.drop('SALE DATE', axis = 1)
 data = data.drop('ADDRESS', axis = 1)
+
 # Drop missing target value & outliers
 data = data[data['SALE PRICE'] != ' -  ']
 tmp = pd.to_numeric(data['SALE PRICE'], downcast = 'signed')
 for i in tmp.index:
     if tmp[i] <= 10:
         data = data.drop(i)
+        
 # Filling missing value
 # Fill 'BUILDING CLASS AT PRESENT' with mode
 tmp = data['BUILDING CLASS AT PRESENT']
@@ -32,12 +34,14 @@ tmp = tmp.replace(to_replace = ' ', value = np.nan)
 tmp = tmp.fillna(data['BUILDING CLASS AT PRESENT'].mode())
 data = data.drop('BUILDING CLASS AT PRESENT', axis = 1)
 data = data.join(tmp)
+
 # Fill 'TAX CLASS AT PRESENT' with mode
 tmp = data['TAX CLASS AT PRESENT']
 tmp = tmp.replace(to_replace = ' ', value = np.nan)
 tmp = tmp.fillna(data['BUILDING CLASS AT PRESENT'].mode())
 data = data.drop('TAX CLASS AT PRESENT', axis = 1)
 data = data.join(tmp)
+
 # Fill 'LAND SQUARE FEET' with mean
 tmp  = data['LAND SQUARE FEET']
 tmp = tmp.replace(to_replace = ' -  ', value = np.nan)
@@ -45,6 +49,7 @@ tmp = pd.to_numeric(tmp, downcast='signed')
 tmp = tmp.fillna(tmp.mean())
 data = data.drop('LAND SQUARE FEET', axis = 1)
 data = data.join(tmp)
+
 # Fill 'GROSS SQUARE FEET' with mean
 tmp = data['GROSS SQUARE FEET']
 tmp = tmp.replace(to_replace = ' -  ', value = np.nan)
@@ -62,21 +67,25 @@ le.fit(data['NEIGHBORHOOD'])
 tmp = pd.Series(le.transform(data['NEIGHBORHOOD']), name = 'NEIGHBORHOOD')
 data = data.drop('NEIGHBORHOOD', axis = 1)
 data = data.join(tmp)
+
 # Transfer 'BUILDING CLASS CATEGORY' variable
 le.fit(data['BUILDING CLASS CATEGORY'])
 tmp = pd.Series(le.transform(data['BUILDING CLASS CATEGORY']), name = 'BUILDING CLASS CATEGORY')
 data = data.drop('BUILDING CLASS CATEGORY', axis = 1)
 data = data.join(tmp)
+
 # Transfer 'BUILDING CLASS AT PRESENT' variable
 le.fit(data['BUILDING CLASS AT PRESENT'])
 tmp = pd.Series(le.transform(data['BUILDING CLASS AT PRESENT']), name = 'BUILDING CLASS AT PRESENT')
 data = data.drop('BUILDING CLASS AT PRESENT', axis = 1)
 data = data.join(tmp)
+
 # Transfer 'BUILDING CLASS AT TIME OF SALE' variable
 le.fit(data['BUILDING CLASS AT TIME OF SALE'])
 tmp = pd.Series(le.transform(data['BUILDING CLASS AT TIME OF SALE']), name = 'BUILDING CLASS AT TIME OF SALE')
 data = data.drop('BUILDING CLASS AT TIME OF SALE', axis = 1)
 data = data.join(tmp)
+
 # Transfer 'TAX CLASS AT PRESENT' variable
 le.fit(data['TAX CLASS AT PRESENT'])
 tmp = pd.Series(le.transform(data['TAX CLASS AT PRESENT']), name = 'TAX CLASS AT PRESENT')
--- HW2_Regression/Random_Forests.py
+++ HW2_Regression/Random_Forests.py
@@ -5,17 +5,54 @@ Created on Mon Oct 16 17:50:28 2017
 
 @author: sitibanc
 """
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 import pandas as pd
 
 # Read Data
-data = pd.read_csv('nyc-rolling-sales-processed.csv')
-data = data.drop(data.columns[[0]], axis = 1)
+rdata = pd.read_csv('nyc-rolling-sales-processed.csv')
+rdata = rdata.dropna()
+data = rdata.drop(rdata.columns[[0]], axis = 1)
 
-# Split Features & Targets
-targets = data['SALE PRICE']
-features = data.drop('SALE PRICE', axis = 1)
+
+# Improve Model By Dropping Columns
+#data = data.drop('BOROUGH', axis = 1)
+data = data.drop('NEIGHBORHOOD', axis = 1)
+data = data.drop('BLOCK', axis = 1)
+#data = data.drop('LOT', axis = 1)
+data = data.drop('ZIP CODE', axis = 1)
+data = data.drop('RESIDENTIAL UNITS', axis = 1)
+#data = data.drop('COMMERCIAL UNITS', axis = 1)
+data = data.drop('TOTAL UNITS', axis = 1)
+#data = data.drop('YEAR BUILT', axis = 1)
+#data = data.drop('TAX CLASS AT TIME OF SALE', axis = 1)
+#data = data.drop('TAX CLASS AT PRESENT', axis = 1)
+data = data.drop('BUILDING CLASS CATEGORY', axis = 1)
+data = data.drop('BUILDING CLASS AT TIME OF SALE', axis = 1)
+#data = data.drop('BUILDING CLASS AT PRESENT', axis = 1)
+data = data.drop('LAND SQUARE FEET', axis = 1)
+#data = data.drop('GROSS SQUARE FEET', axis = 1)
+
 
 # Split Training & Testing Data
-trainX, testX, trainY, testY = train_test_split(targets, features, test_size = 0.25)
+train = data[:int(len(data) * 0.95)]
+test = data[int(len(data) * 0.95):]
+
+# Split Features & Targets
+trainY = train['SALE PRICE']
+trainX = train.drop('SALE PRICE', axis = 1)
+testY = test['SALE PRICE']
+testX = test.drop('SALE PRICE', axis = 1)
+
+# Create Random Forest Regressor object
+regr = RandomForestRegressor()
+
+# Train the model using the training sets
+regr.fit(trainX, trainY)
+
+# Make predictions using the testing set
+predY = regr.predict(testX)
+
+# Calculate Mean Absolute Error
+MAE = mean_absolute_error(testY, predY)
+print('MAE:', MAE)
