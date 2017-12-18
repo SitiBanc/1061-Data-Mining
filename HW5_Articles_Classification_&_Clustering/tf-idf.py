1c44109d08147c95878df7053d75256e115f8928
Hsin-Yi Chen <hychen.iim05g@g2.nctu.edu.tw>
2017-12-18 19:27:26 +0800

    HW5 Done
    Changed
    - 更新tf-idf.py（加入分群與分類）
    Added
    - HW5報告文件Readme.pdf

--- HW5_Articles_Classification_&_Clustering/tf-idf.py
+++ HW5_Articles_Classification_&_Clustering/tf-idf.py
@@ -7,6 +7,9 @@ Created on Mon Dec 18 15:23:47 2017
 """
 import pandas as pd
 from jieba import analyse
+from sklearn.cluster import KMeans
+from sklearn import tree, metrics
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # =============================================================================
@@ -19,7 +22,7 @@ label = data['mainTag']
 analyse.set_stop_words('stop_words.txt')
 # Cutting Articles
 for i in range(len(data)):
-    seg_list = analyse.extract_tags(data['postContent'][i])
+    seg_list = analyse.extract_tags(data['postContent'][i], topK = 30)
     datastr = ''
     for t in seg_list:
         datastr += t + ' '
@@ -31,7 +34,21 @@ X = vectorizer.fit_transform(corpus).toarray()
 # =============================================================================
 # Clustering (K-Means, k = 5)
 # =============================================================================
-
+kmeans = KMeans(5).fit(X)
+# Print Out Result
+print('K-Means Clustering Result:')
+print(kmeans.labels_)
 # =============================================================================
 # Classification (using Decision Tree)
 # =============================================================================
+# Spliting Training and Teating Data
+trainX, testX, trainY, testY = train_test_split(X, label, test_size = 0.25)
+# Build decision tree
+clf = tree.DecisionTreeClassifier(max_depth = 10)
+clf = clf.fit(trainX, trainY)
+# Calculate the accuracy
+testY_predicted = clf.predict(testX)
+accuracy = metrics.accuracy_score(testY, testY_predicted)
+# Print Out Result
+print('\nDecision Tree Classification Result:')
+print('Accuracy:', accuracy * 100, '%')
